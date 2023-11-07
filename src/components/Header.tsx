d2eab7c44e97a9f2609b581c4f588b509d2794cc
Ayush Mehrotra <ayushmehrotra099@gmail.com>
2023-11-07 11:40:23 +0530

    Added Layout Section and imporved Header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -22,7 +22,7 @@ const menuItems = [
 export default function Header() {
   const { authStatus } = useAuth();
   return (
-    <div className="relative w-full bg-white py-2">
+    <div className="relative sm:text-black md:text-black text-black w-full bg-white py-2">
       <div className="mx-auto flex max-w-7xl items-center justify-between px-4 py-2 sm:px-6 lg:px-8">
         <div className="inline-flex items-center space-x-2">
           <Link href={"/"} className="inline-block w-full max-w-[150px]">
